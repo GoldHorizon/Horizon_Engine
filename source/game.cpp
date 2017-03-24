812a123a518d29d8ccbbcfb8d948585b7e3601d9
GoldHorizon <francken13@yahoo.com>
2017-03-23 22:47:08 -0700

    Removed testing code, prepared to merge

--- source/game.cpp
+++ source/game.cpp
@@ -69,20 +69,6 @@ int Game::Initialize()
 	// If all is well, set the game state and return something besides -1
 	ChangeState(GameState::MAIN_MENU);
 
-	// MAJOR TEMP ENTITY TESTS
-	Entity* playButtonTEMP = new Entity;
-	playButtonTEMP->LoadFromFile("images/PlayButton.png");
-	playButtonTEMP->SetImageOrigin(playButtonTEMP->imageWidth() / 2, playButtonTEMP->imageHeight() / 2);
-	playButtonTEMP->SetPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3);
-
-	Entity* quitButtonTEMP = new Entity;
-	quitButtonTEMP->LoadFromFile("images/QuitButton.png");
-	quitButtonTEMP->SetImageOrigin(quitButtonTEMP->imageWidth() / 2, quitButtonTEMP->imageHeight() / 2);
-	quitButtonTEMP->SetPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 2 / 3);
-
-	Entities().AddEntity("PlayButton", playButtonTEMP);
-	Entities().AddEntity("QuitButton", quitButtonTEMP);
-
 	return 0;
 }
 
