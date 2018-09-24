273cbc9483c44ce4b456065c4ad43f5c1d261a33
Nick Francke <nickfrancke13@gmail.com>
2018-09-24 16:32:25 -0700

    Remove extra from include lines

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#include "../include/game.h"
-#include "../include/constants.h"
+#include "game.h"
+#include "constants.h"
 
 // Program start
 int main(int argc, char** argv)
