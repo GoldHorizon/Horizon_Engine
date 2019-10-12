6bb7dd6078368b833bc696b6fa9775920bb80398
Nicholas Francke <nickfrancke13@gmail.com>
2019-10-12 18:08:58 +1300

    Implement frame counter and frame limiter

--- include/constants.h
+++ include/constants.h
@@ -9,15 +9,6 @@ const SDL_Color DEFAULT_COLOR = { 0, 0, 0 };
 
 const int FONT_COUNT = 4;
 
-// Used in variable step game loop, not sure if chrono uses MS though...
-// http://gameprogrammingpatterns.com/game-loop.html
-//const double MS_PER_UPDATE = 0.0005;
-const unsigned TICKS_PER_SECOND = 30;				// Game ticks per second - should be MUCH less than render frames
-const float SKIP_TICKS = 1000 / TICKS_PER_SECOND;	//
-const int MAX_FRAMESKIP = 5;						//
-
-const unsigned MAX_FPS = 120;
-const float SKIP_RENDERS = 1000 / MAX_FPS;
 
 // Math Constants
 const float PI = 3.14159265;
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
 #include "game.h"
-#include "constants.h"
 #include "globals.h"
+
+// Used in variable step game loop, not sure if chrono uses MS though...
+// http://gameprogrammingpatterns.com/game-loop.html
+const unsigned TICKS_PER_SECOND = 30;				// Game ticks per second - should be less than render frames
+const float SKIP_TICKS = 1000 / TICKS_PER_SECOND;
+const int MAX_FRAMESKIP = 5;
+
+const unsigned MAX_FPS = 60;
+const float SKIP_RENDERS = 1000 / MAX_FPS;
 
 #ifdef _WIN32
 	#ifdef _WIN64
@@ -32,12 +40,14 @@ int main(int argc, char** argv)
 
     // Start tick counter and frame counter
 	unsigned next_game_tick = SDL_GetTicks();
+	unsigned next_render = SDL_GetTicks();
 	unsigned frames = 0;
 
 	unsigned last_frame_update = next_game_tick;
 
     // Variable used to track how many updates happened
-	int loops;
+	unsigned update_loops;
+	unsigned render_loops;
 
     // Variable used to keep track of how much to accomodate for processor speed
 	float interpolation;
@@ -47,10 +57,10 @@ int main(int argc, char** argv)
 
 	while (game_is_running)
 	{
-		loops = 0;
+		update_loops = 0;
 
 		// Check if we have to process things before the next frame
-		while (SDL_GetTicks() > next_game_tick && loops < MAX_FRAMESKIP && game_is_running)
+		while (SDL_GetTicks() > next_game_tick && update_loops < MAX_FRAMESKIP && game_is_running)
 		{
             // Get input from keyboard and mouse
 			game_is_running = (main_game.GetInput() == GameStatus::running);
@@ -60,14 +70,22 @@ int main(int argc, char** argv)
 
             // Skip ticks and add loops until we have processed up to the next frame
 			next_game_tick += SKIP_TICKS;
-			loops++;
+			++update_loops;
 		}
 
-		// Decides how much to predict movement of entity -- @todo rework this?
-		interpolation = float(SDL_GetTicks() + SKIP_TICKS - next_game_tick) / float(SKIP_TICKS);
+		render_loops = 0;
 
-		main_game.Render(interpolation);
-		++frames;
+		while (SDL_GetTicks() > next_render && render_loops < MAX_FRAMESKIP && game_is_running)
+		{
+			// Decides how much to predict movement of entity
+			interpolation = float(SDL_GetTicks() + SKIP_TICKS - next_game_tick) / float(SKIP_TICKS);
+
+			main_game.Render(interpolation);
+			++frames;
+
+			next_render += SKIP_RENDERS;
+			++render_loops;
+		}
 
 		unsigned current_tick = SDL_GetTicks();
 		if (current_tick - last_frame_update > 1000) {
